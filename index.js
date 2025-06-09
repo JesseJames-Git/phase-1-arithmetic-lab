4e64c324c5ffe2585e075a62f76064e4edb7c337
Jesse <jessejamesjj007@gmail.com>
2025-06-09 11:36:02 +0300

    Add index.js content

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+const num1 = 31 
+const num2 = 2
+const multiply = num1 * num2
+
+console.log(multiply)
+
+// Multiplication 
+
+Math.round (Math.random() * 10 )
+
+//Generates random number greater than zero
+
+const num3 = 10 
+const num4 = 6
+let mod = num3 % num4
+
+console.log(mod)
+
+//Modulus of two variables giving 4
+
+let max = Math.max(5,10,15,20)
+
+console.log(max)
+//Max number
